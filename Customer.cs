74482ec9ca40f38e85720b744a9076a01f5fa05c
Geffersones <ilyaofficial1235@gmail.com>
2021-03-21 15:14:47 +0300

    Alfa 0.1

---
+++ Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barbershop
+{
+    class Customer
+    {
+        private int cur_max_ID = 0;
+        private int ID;
+        public string name;  
+        public string last_name;
+        
+
+        public Customer(string _name, string _last_name)
+        {
+            name = _name;
+            last_name = _last_name;
+            ID = cur_max_ID;
+            cur_max_ID++;
+        }
+        public void print()
+        {
+            Console.WriteLine("_______________");
+            Console.WriteLine("Клиент № " + ID + ":");
+            Console.WriteLine(name + " " + last_name);
+            Console.WriteLine("_______________");
+        }
+}
+
+    }
+}
